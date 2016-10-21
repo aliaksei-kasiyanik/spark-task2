b87251017289c22ccbab09c06f59875e38858d51
Aliaksei Kasiyanik <aleksey.kasianik@gmail.com>
2016-10-21 19:49:32 +0300

    output files

--- Task2.scala
+++ Task2.scala
@@ -9,6 +9,6 @@ object Task2 {
   def main(args: Array[String]) {
     val sc = new SparkContext(new SparkConf().setAppName("Task2"))
 
-    sc.textFile(args(0)).map(line => (line.split("\\s+")(1), line.split("\\s+")(2))).reduceByKey(_ + _).map(f => f._1+"\t"+f._2).saveAsTextFile("hdfs://localhost:8020" + args(1))
+    sc.textFile("hdfs://localhost:8020" + args(0)).map(line => (line.split("\\s+")(1), line.split("\\s+")(2).toInt)).reduceByKey(_ + _).map(f => f._1+"\t"+f._2).saveAsTextFile("hdfs://localhost:8020" + args(1))
   }
 }
